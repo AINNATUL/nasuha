2554ad7be7e57d87591f2a2f8a696eea9dac48b5
AINNATUL <natulsuha38@gmail.com>
2019-08-07 16:02:13 -0700

    cubaan pertama

---
+++ biodata/biodata/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        Form3.Show()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Hide()
+    End Sub
+End Class
